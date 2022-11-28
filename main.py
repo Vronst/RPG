1eafabf7a1ac34c7b4e6bf5c8f80a46693d8d0e2
Vronst <adam199914@gmail.com>
2022-11-28 15:56:54 +0100

    Dodanie base funkcji

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from staty import NPC
 import sqlite3
+import random
 
+
+p1 = []  # postacie
 
 polaczenie = sqlite3.connect('list_npc.db')
 
@@ -8,25 +11,85 @@ kursor = polaczenie.cursor()
 
 kursor.execute("""
 CREATE TABLE IF NOT EXISTS npcs (
-        fr TEXT,
-        name TEXT,
-        hp INTEGER,
-        ll INTEGER,
-        rl INTEGER,
-        ch INTEGER,
-        la INTEGER,
-        ra INTEGER,
-        he INTEGER,
-        WS INTEGER,
-        race TEXT,
-        BS INTEGER,
-        S INTEGER,
-        T INTEGER,
-        I INTEGER,
-        Ag INTEGER,
-        Dex INTEGER,
-        Int INTEGER,
-        WP INTEGER,
-        Fel INTEGER
+        fr TEXT DEFAULT 'default',
+        name TEXT DEFAULT 'Paolo',
+        hp INTEGER DEFAULT 1,
+        ll INTEGER DEFAULT 1,
+        rl INTEGER DEFAULT 1,
+        ch INTEGER DEFAULT 1,
+        la INTEGER DEFAULT 1,
+        ra INTEGER DEFAULT 1,
+        he INTEGER DEFAULT 1,
+        WS INTEGER DEFAULT 1,
+        race TEXT DEFAULT 'Human',
+        BS INTEGER DEFAULT 1,
+        S INTEGER DEFAULT 1,
+        T INTEGER DEFAULT 1,
+        I INTEGER DEFAULT 1,
+        Ag INTEGER DEFAULT 1,
+        Dex INTEGER DEFAULT 1,
+        Int INTEGER DEFAULT 1,
+        WP INTEGER DEFAULT 1,
+        Fel INTEGER DEFAULT 1
 """)
 polaczenie.commit()
+
+
+def add_npc():
+
+    b0 = str(input("Fratcion: "))
+    b1 = str(input("Name: "))
+    b2 = str(input("Race: "))
+    b3 = int(input("HP: "))
+    b4 = int(input("Left leg armor: "))
+    b5 = int(input("Right leg armor: "))
+    b6 = int(input("Left arm armor: "))
+    b7 = int(input("Right arm armor: "))
+    b8 = int(input("Chest armor: "))
+    b9 = int(input("Head armor: "))
+    b10 = int(input("WS: "))
+    b11 = int(input("BS: "))
+    b12 = int(input("S: "))
+    b13 = int(input("T: "))
+    b14 = int(input("I: "))
+    b15 = int(input("AG: "))
+    b16 = int(input("Dex: "))
+    b17 = int(input("Int: "))
+    b18 = int(input("WP: "))
+    b19 = int(input("Fel: "))
+    NPC({b0}, {b1}, {b2}, {b3}, {b4}, {b5}, {b6}, {b7}, {b8}, {b9}, {b10}, {b11}, {b12}, {b13}, {b14}, {b15}, {b16}, {b17}, {b18}, {b19})
+
+    kursor.execute("""
+    INSERT INTO npc VALUES
+    ({},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{})
+    """.format(b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19))
+    polaczenie.commit()
+
+
+def load_npc():
+
+    search = input("Type name of fraction: \n")
+    number_of = input("Random (R) or All (A)\n").upper()
+    kursor.execute("""
+    SELECT * FROM npcs
+    WHERE fr = {}
+    """.format(search))
+    if number_of == 'A':
+        druk = kursor.fetchall()
+        print(druk)
+    elif number_of == 'R':
+        x = int(input("Max number: \n"))
+        druk = kursor.fetchone()
+        ilosc = random.randint(1, x)
+        while ilosc > 0:
+            print(druk)
+            ilosc -= 1
+
+while True:
+    choice = input("A: Add NPC\tB: Show NPC\nE: exit").upper()
+    if choice == 'A':
+        add_npc()
+    elif choice == 'B':
+        load_npc()
+    elif choice == 'E':
+        break
--- staty.py
+++ staty.py
@@ -1,7 +1,27 @@
-
+#import sqlite3
 
 class NPC:
 
-
-    def __init__(self):
-        self.
+    def __init__(self, fr="", name="", race="", hp=1, ll=0, rl=0, ch=0, la=0, ra=0, he=0, WS=1, BS=1, S=1, T=1, I=1, Ag=1, Dex=1, Int=1, WP=1, Fel=1):
+        self.fr = fr
+        self.name = name
+        self.hp = hp
+        self.ll = ll
+        self.rl = rl
+        self.ch = ch
+        self.la = la
+        self.ra = ra
+        self.he= he
+        self.WS = WS
+        self.race = race
+        self.BS = BS
+        self.S = S
+        self.T = T
+        self.i = I
+        self.Ag = Ag
+        self.Dex = Dex
+        self.Int = Int
+        self.WP = WP
+        self.Fel = Fel
+        #self.connection = sqlite3.connect('list_npc.db')
+        #self.cursor = self.connection.cursor()
